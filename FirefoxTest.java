85ca32bc7053bd4977922827612ce9a7c66fa08f
Henry Qin <root@hq6.me>
2018-08-21 10:54:59 -0700

    Generate a click at the center of the element

--- FirefoxTest.java
+++ FirefoxTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.Dimension;
 
 public class FirefoxTest {
 	public static void main(String[] args) {
@@ -12,7 +14,12 @@ public class FirefoxTest {
 		driver.get("http://www.google.com");
         JavascriptExecutor js = (JavascriptExecutor) driver;
         WebElement element = new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
-        System.out.println(element.getLocation());
         js.executeScript("document.body.onclick = function(e) {console.log(e);}");
+        Point loc = element.getLocation();
+        Dimension size = element.getSize();
+        System.out.println("Location: " + loc);
+        System.out.println("Size:     " + size);
+        System.out.printf("Midpoint:    (%d,%d)\n", loc.getX() + size.getWidth() / 2, loc.getY() + size.getHeight() / 2);
+        element.click();
 	}
 }
