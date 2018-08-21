8c43ce3c13b40cb0db82847e449390e58c64ad0b
Henry Qin <root@hq6.me>
2018-08-21 00:30:26 -0700

    Simple example of executing Javascript in browser

--- FirefoxTest.java
+++ FirefoxTest.java
@@ -1,9 +1,12 @@
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 public class FirefoxTest {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("alert('hello world from seleinium!');");
 	}
 }
