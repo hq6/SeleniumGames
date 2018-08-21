a0270b42007c8bc02a0bed8abb96ec77c41a801e
Henry Qin <root@hq6.me>
2018-08-21 14:11:24 -0700

    Center the dot on the click.

--- js/add_onclick.js
+++ js/add_onclick.js
@@ -1,6 +1,6 @@
 window.dot=document.getElementById(arguments[0]);
 document.body.onclick = function(e) {
     console.log(e);
-    dot.style.left = e.clientX + 'px';
-    dot.style.top = e.clientY + 'px';
+    dot.style.left = (e.clientX - dot.width / 2) + 'px';
+    dot.style.top = (e.clientY - dot.height / 2) + 'px';
 };
