22fe754628c668d72c342111850f7c13eabe75c2
Henry Qin <root@hq6.me>
2018-08-21 10:11:34 -0700

    Wait for an element to appear and then print its coordinates
    Next step is to try clicking on the element.

--- FirefoxTest.java
+++ FirefoxTest.java
@@ -1,12 +1,18 @@
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class FirefoxTest {
 	public static void main(String[] args) {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("http://www.google.com");
         JavascriptExecutor js = (JavascriptExecutor) driver;
+        WebElement element = new WebDriverWait(driver, 10).until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+        System.out.println(element.getLocation());
         js.executeScript("document.body.onclick = function(e) {console.log(e);}");
 	}
 }
